713a7631b3f2f2c5a4504537b5d84320c7f07ac6
dmitry <arbuzov.mail@gmail.com>
2018-10-15 20:30:30 +0300

    4.create_controllers

---
+++ app/code/community/Fox/News/controllers/CustomerController.php
@@ -0,0 +1,9 @@
+<?php
+
+class Fox_News_CustomerController extends Mage_Core_Controller_Front_Action {
+
+    public function mytestAction()
+    {
+        die('mytestAction Customer');
+    }
+}
---
+++ app/code/community/Fox/News/controllers/ExternalController.php
@@ -0,0 +1,9 @@
+<?php
+
+class Fox_News_ExternalController extends Mage_Core_Controller_Front_Action {
+
+    public function mytestAction()
+    {
+        die('mytestAction External');
+    }
+}
